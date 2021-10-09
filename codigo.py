344bac1c408a32440b3124258f3b6995d6619394
DiegoF <diego.ledesma@correounivalle.edu.co>
2021-10-08 19:45:56 -0500

    Ya corre la optimización de prueba

--- codigo.py
+++ codigo.py
@@ -5,6 +5,9 @@ import cv2
 import numpy as npy
 from matplotlib import pyplot as plt 
 
+from scipy.optimize import minimize
+
+from numpy import random
 
 #print(cv2.__version__)
 
@@ -67,28 +70,7 @@ print("")
 
 
 
-"""
-mExample = npy.zeros([5, 6], dtype = float)
-print(mExample)
 
-m1 = npy.array([  [1, 1, 1, 1, 1, 1], 
-				  [2, 2, 2, 2, 2, 2], 
-				  [3, 6, 3, 3, 3, 3],
-				  [4, 4, 4, 4, 4, 4], 
-				  [5, 5, 5, 5, 5, 5]])
-print(m1)
-
-col0 = npy.zeros([5, 1], dtype = float)
-
-mExample[:,2] = m1[:,1]
-mExample[:,2] += m1[:,0]
-mExample[:,2] = mExample[:,2]/2
-print(mExample)
-
-col0 = mExample[:,2]
-col0 += mExample[:,2]
-print(col0)
-"""
 
 
 
@@ -121,9 +103,10 @@ S = npy.zeros([5, 49], dtype = float)
 V = npy.zeros([5,245], dtype = float)  
 
 for i in range(49):
-	#U[:,i*5:(i+1)*5], S[:,i*5:(i+1)*5], V[:,i*5:(i+1)*5] = npy.linalg.svd(MTraining[:, i*5: (i+1)*5], full_matrices = False)
+	#U, S, V = npy.linalg.svd(MTraining[:, i*5: (i+1)*5], full_matrices = False)
 	U[:,i*5:(i+1)*5], S[:,i], V[:,i*5:(i+1)*5] = npy.linalg.svd(MTraining[:, i*5: (i+1)*5], full_matrices = False)
 
+"""
 #Visualización del rostro promedio
 print(U.shape)
 print(S.shape)
@@ -133,17 +116,86 @@ print(S[:,16])
 print(S.shape)
 
 #print(V[:,10:15])
+"""
+
 
 
 """
-huellaPropia = U[:,5].reshape(n,m)
-plt.imshow(huellaPropia,cmap ='gray')
-plt.title('HuellaPropiaHP1')
+#Visualización de las "huellas base" 
+huellaBase = U[:,5].reshape(n,m)
+plt.imshow(huellaBase,cmap ='gray')
+plt.title('HuellaBase1')
 plt.xticks([]),plt.yticks([])
 plt.show()
 """
 
 
 
+
+mExample = npy.zeros([5, 6], dtype = float)
+#print(mExample)
+
+m1 = npy.array([  [1, 1, 1, 1, 1, 1],      
+				  [2, 2, 2, 2, 2, 2], 
+				  [3, 6, 3, 3, 3, 3],
+				  [4, 4, 4, 4, 4, 4], 
+				  [5, 5, 5, 5, 5, 5]])
+
+"""
+x0*c0+x1*c1+x2*c2
+				  [[2]
+				  [3]
+				  [18]
+				  [15]
+				  [2]]
+"""
+#print(m1)
+
+col0 = npy.zeros([5, 1], dtype = float)
+"""
+mExample[:,2] = m1[:,1]
+mExample[:,2] += m1[:,0]
+mExample[:,2] = mExample[:,2]/2
+print(mExample)
+col0 = mExample[:,2]
+col0 += mExample[:,2]
+print(col0)
+"""
+#tv = npy.array([[2], [4], [6], [1], [3]])
+#print(tv)
+
+a0 = m1[:,0]
+a1 = m1[:,1]
+a2 = m1[:,2]
+
+vecPrueba = npy.array([[2], [3], [18], [15], [2]])
+
+def objective_fn(x):
+	x0 = x[0]
+	x1 = x[1]
+	x2 = x[2]
+	dist = npy.linalg.norm(vecPrueba-(x0*a0 + x1*a1 + x2*a2 ))
+	return dist
+
+"""
+print(npy.max(U))#0.9999428217733894
+print(npy.min(U))#-0.030018814508134515
+print(npy.max(U) - npy.min(U))#1.029961636281524
+print(255.0/(npy.max(U) - npy.min(U)))#247.58203705589253
+
+
+startingX = npy.array([[(random.rand()*1.029961636281524) - 0.030018814508134515], 
+						[(random.rand()*1.029961636281524) - 0.030018814508134515], 
+						[(random.rand()*1.029961636281524) - 0.030018814508134515]]) 
+print(startingX)
+"""
+
+startingJ = npy.array([[random.randint(10)], 
+						[random.randint(10)], 
+						[random.randint(10)]])
+print(startingJ)
+
+result = minimize(objective_fn, startingJ, method = 'SLSQP')
+print(result)
 
 
